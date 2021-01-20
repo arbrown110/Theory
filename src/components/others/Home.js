82d798da84a2eebd7932b8713f85c4a74f4c9055
Angel_Brown <arbrown110@gmail.com>
2021-01-20 00:33:11 -0500

    other files creation.js.

---
+++ src/components/others/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import React from 'react'
+import AdventuresContainer from '../containers/AdventuresContainer'
+
+
+class Home extends React.Component{
+
+    render(){
+
+        return (
+            <div>
+                <AdventuresContainer />
+            </div>
+        )
+    }
+}
+
+export default Home
---
+++ src/components/others/about.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div>
+            <h1>The journey back</h1>
+            <p>
+            This is a little of my experience going to the country of Sierre Leone. 
+            </p>
+            <p>
+            Follow me on my journey of how I navigating the country with the people , culture and food.
+            </p>
+            
+            <p>-sierra leone phrase</p>
+     
+        </div>
+    )
+}
