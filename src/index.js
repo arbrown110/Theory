9bc94f73502d2b205125112d9faeed1bdb418030
Angel_Brown <arbrown110@gmail.com>
2021-01-20 00:39:03 -0500

    edit to index.js.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
-import { Provider } from 'react-redux';
-import store from './store'
+import { createStore, applyMiddleware} from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import  { rootReducer } from './reducers/rootReducer'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
+    <Router>
     <App />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
