e5f1df1cef8ba7fc452309005f8fee04660be0b3
Angel_Brown <arbrown110@gmail.com>
2021-01-20 00:14:34 -0500

    creation of Events.js.

---
+++ src/components/Events.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import EventUnique from '../components/EventUnique'
+
+
+function Events(props) {
+ return (
+     <div>
+         <h3>What has happened so far:</h3>
+         {props.events && props.events.map(event => 
+             <Event id={event.id} title={event.title} description={event.description} krio={event.krio} adventureId={event.adventure_id}/>      
+         )}
+     </div>
+ )
+    
+}
+ export default Events
