176ca9a5db5f6932f2c6940367246256839a1b3a
Angel_Brown <arbrown110@gmail.com>
2021-01-19 23:11:26 -0500

    creation of AdventureUnique component.

---
+++ src/components/AdventureUnique.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import EventsContainer from '../containers/EventsContainer'
+import EventForm from '../components/EventForm'
+
+const AdventureUnique = (props) => {
+  let adventure = props.adventures.find(
+    (adventure) => adventure.id == props.match.params.id
+  )
+
+  return (
+    <div className='adventureShowPage'>
+      <div className='adventurePageImage'>
+        {adventure ? (
+          <img
+            src={adventure.image_url}
+            alt='adventure displayed '
+            max-width='500px'
+            max-height='500px'
+          />
+        ) : null}
+      </div>
+      <div className='adventurePageDetails'>
+        <h1>{adventure ? adventure.name : null}</h1>
+      </div>
+      <EventForm adventure={adventure} />
+      <EventsContainer adventure={adventure} />
+    </div>
+  )
+}
+
+export default AdventureUnique
